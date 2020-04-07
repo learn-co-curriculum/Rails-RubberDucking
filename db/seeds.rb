71fcda216d4ee5397c650502bec39a9e3b9c35e2
Maxwell Benton <maxwellbenton@gmail.com>
2020-04-07 17:04:17 -0400

    update seed.rb duck creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 end
 
 5.times do
-  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed)
+  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed)
 end
